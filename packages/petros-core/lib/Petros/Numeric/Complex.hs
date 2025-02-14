4b7cd3d5fd805db2a284cf2b1562cf277683e127
Burtannia <james@functionalcore.dev>
2025-02-14 09:56:36 +0000

    feat: Numeric.Complex module

---
+++ packages/petros-core/lib/Petros/Numeric/Complex.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE Safe #-}
+
+module Petros.Numeric.Complex
+    ( module Data.Complex
+    ) where
+
+import Data.Complex
+    ( Complex (..)
+    , cis
+    , conjugate
+    , imagPart
+    , magnitude
+    , mkPolar
+    , phase
+    , polar
+    , realPart
+    )
