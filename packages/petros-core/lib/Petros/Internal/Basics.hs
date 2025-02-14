5286523196d82a8f08dc93e2210aee150b4cb17c
Burtannia <james@functionalcore.dev>
2025-02-14 08:13:52 +0000

    feat (internal): add basics module

---
+++ packages/petros-core/lib/Petros/Internal/Basics.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE Safe #-}
+
+module Petros.Internal.Basics
+    ( module X
+    ) where
+
+import Prelude as X
+    ( Applicative (..)
+    , Bool (..)
+    , Char
+    , Double
+    , Either (..)
+    , Eq (..)
+    , Float
+    , Functor (..)
+    , Int
+    , Integer
+    , Maybe (..)
+    , Monad (..)
+    , Num (..)
+    , Ord (..)
+    , String
+    , fst
+    , otherwise
+    , snd
+    , ($)
+    , (.)
+    , (<$>)
+    )
