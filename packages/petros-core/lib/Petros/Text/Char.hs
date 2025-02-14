ea2d5c3b08c25e26bebfb1e8bc3f88eb9aa31ee8
Burtannia <james@functionalcore.dev>
2025-02-14 08:37:14 +0000

    feat: add Text.Char module

---
+++ packages/petros-core/lib/Petros/Text/Char.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE Safe #-}
+
+module Petros.Text.Char
+    ( module Data.Char
+    ) where
+
+import Data.Char
+    ( Char
+    , GeneralCategory (..)
+    , generalCategory
+    , isAlpha
+    , isAlphaNum
+    , isAscii
+    , isAsciiLower
+    , isAsciiUpper
+    , isControl
+    , isDigit
+    , isHexDigit
+    , isLatin1
+    , isLetter
+    , isLower
+    , isLowerCase
+    , isMark
+    , isNumber
+    , isOctDigit
+    , isPrint
+    , isPunctuation
+    , isSeparator
+    , isSpace
+    , isSymbol
+    , isUpper
+    , isUpperCase
+    , toLower
+    , toTitle
+    , toUpper
+    )
+
